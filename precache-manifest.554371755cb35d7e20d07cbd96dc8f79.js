d50740ea0b73d08ead36f7f3f8c385f9bab8e2ff
sarangkartikey50 <sarangkartikey50@gmail.com>
2020-08-29 20:05:17 +0530

    Updates

---
+++ precache-manifest.554371755cb35d7e20d07cbd96dc8f79.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3d24eb433bdff74a1039ba6b7ac9dd",
+    "url": "/drawing-board/index.html"
+  },
+  {
+    "revision": "c61551e8a7b97332d075",
+    "url": "/drawing-board/static/css/3.d99079a5.chunk.css"
+  },
+  {
+    "revision": "9179dea9aca1ae2b34b3",
+    "url": "/drawing-board/static/css/4.7a0dc2a6.chunk.css"
+  },
+  {
+    "revision": "fe4e70e7835966fc0d1f",
+    "url": "/drawing-board/static/css/5.e1018018.chunk.css"
+  },
+  {
+    "revision": "db4360b3981cd57fcb4b",
+    "url": "/drawing-board/static/css/6.26469add.chunk.css"
+  },
+  {
+    "revision": "935f3ce9f591ead60d35",
+    "url": "/drawing-board/static/css/main.567751cd.chunk.css"
+  },
+  {
+    "revision": "444cc0fab02d437ecfc5",
+    "url": "/drawing-board/static/js/2.53b14922.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/drawing-board/static/js/2.53b14922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61551e8a7b97332d075",
+    "url": "/drawing-board/static/js/3.62050a51.chunk.js"
+  },
+  {
+    "revision": "9179dea9aca1ae2b34b3",
+    "url": "/drawing-board/static/js/4.bb2b5fdb.chunk.js"
+  },
+  {
+    "revision": "fe4e70e7835966fc0d1f",
+    "url": "/drawing-board/static/js/5.219578ec.chunk.js"
+  },
+  {
+    "revision": "db4360b3981cd57fcb4b",
+    "url": "/drawing-board/static/js/6.301f274e.chunk.js"
+  },
+  {
+    "revision": "935f3ce9f591ead60d35",
+    "url": "/drawing-board/static/js/main.57b4fb9a.chunk.js"
+  },
+  {
+    "revision": "a1115e142f7258bc5b28",
+    "url": "/drawing-board/static/js/runtime-main.b9929f02.js"
+  }
+]);
